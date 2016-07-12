4ed439dc587c9ce088bb247e26e066c03f7b4918
kylem038 <misenckj@gmail.com>
2016-07-12 17:31:16 -0600

    Now able to add articles when the bookmark button is hit

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-// Button Variables
-var inputSubmitButton = document.getElementById('input-submit');
-var markReadButton = document.getElementById('mark-read');
-var markRemoveButton = document.getElementById ('mark-remove');
+// jQuery Variables
+var $inputSubmitButton = $('#input-submit');
+var $markReadButton = $('#mark-read');
+var $markRemoveButton = $('#mark-remove');
 // Input Variables
-var inputNewTitle = document.getElementById ('input-title');
-var inputNewURL = document.getElementById ('input-url');
+var $inputNewTitle = $('#input-title');
+var $inputNewURL = $('#input-url');
 // Output Variables
-var outputNewTitle = document.getElementById ('insert-bookmark-title');
-var outputNewURL = document.getElementById ('insert-bookmark-url');
+var $outputNewTitle = $('#insert-bookmark-title');
+var $outputNewURL = $('#insert-bookmark-url');
+// Create New Article
+var $addNewBookmark = $('#add-new-bookmark');
+// var articleContent =
+
+// jQuery
+$inputSubmitButton.on('click', function () {
+  $('#add-new-bookmark').append('<article class="new-bookmark"><li class="new-bookmark-title" id="insert-bookmark-title">Bookmark Title</li><li class="new-bookmark-url" id="insert-bookmark-url">Bookmark URL</li><button type="button" id="mark-read" class="read-bookmark">Read</button><button type="button" id="mark-remove" class="remove-bookmark">Remove</button></article>');
+});
