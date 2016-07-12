d188605b421e67cac53ba6cf314ff968e3817d83
Blake Worsley <blakeworsley@gmail.com>
2016-07-12 15:46:46 -0600

    added variables in javascript and changed articles in html

---
+++ script.js
@@ -0,0 +1,10 @@
+// Button Variables
+var inputSubmitButton = document.getElementById('input-submit');
+var markReadButton = document.getElementById('mark-read');
+var markRemoveButton = document.getElementById('mark-remove');
+// Input Variables
+var inputNewTitle = document.getElementById('input-title');
+var inputNewURL = document.getElementById('input-url');
+// Output Variables
+var outputNewTitle = document.getElementById('insertBookmarkTitle');
+var outputNewURL = document.getElementById('insertBookmarkTitle');
