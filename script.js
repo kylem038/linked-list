5dccd7e7c94852122b63dc106b9b8e480343c30f
Blake Worsley <blakeworsley@gmail.com>
2016-07-12 18:03:05 -0600

    added variables to jquery and initial funciton that takes the user input and creates new articles

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
 // Button Variables
-var inputSubmitButton = document.getElementById('input-submit');
-var markReadButton = document.getElementById('mark-read');
-var markRemoveButton = document.getElementById('mark-remove');
+// var inputSubmitButton = document.getElementById('input-submit');
+var $inputSubmitButton = $('#input-submit')
+var $markReadButton = ('#mark-read')
+var $markRemoveButton = ('#mark-remove')
 // Input Variables
-var inputNewTitle = document.getElementById('input-title');
-var inputNewURL = document.getElementById('input-url');
+var $inputNewTitle = $('#input-title')
+var $inputNewUrl = $('#input-url')
+var $inputNewTitleValue = $inputNewTitle.val()
+var $inputNewUrlValue = $inputNewUrl.val()
 // Output Variables
-var outputNewTitle = document.getElementById('insertBookmarkTitle');
-var outputNewURL = document.getElementById('insertBookmarkTitle');
+var $outputNewTitle = $('#insert-bookmark-title')
+var $outputNewUrl = $('#insert-bookmark-title')
+var $newArticle = $('#add-new-article')
+
+
+//user input title and url
+//clicks inputSubmitButton
+  //take input from both inputNewTitle and inputNewUrl
+  //create new article
+  //insert into new outputNewTitle and outputNewURL
+
+$inputSubmitButton.on('click', function() {
+  $newArticle.append('<article id="add-new-bookmark" class="new-bookmark"><li id="insert-bookmark-title" class="new-bookmark-title">' +
+  $inputNewTitle.val() + // input from the user
+  '</li><li id="insert-bookmark-url" class="new-bookmark-url">' +
+  $inputNewUrl.val() + //input from the user
+  '</li><button id="mark-read" class="read-bookmark" type="button" name="button">Read</button><button id="mark-remove" class="remove-bookmark" type="button" name="button">Remove</button></article>');
+});
+
+//add html for new article
+  //modify article content / title and url
+  //
