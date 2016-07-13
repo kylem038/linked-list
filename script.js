98ee3d2401a41235118507147e1b12809a05cfa7
kylem038 <misenckj@gmail.com>
2016-07-12 18:02:18 -0600

    Added working functionality for pulling inputs and adding bookmarks within the html.

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 // jQuery Variables
-var $inputSubmitButton = $('#input-submit');
-var $markReadButton = $('#mark-read');
-var $markRemoveButton = $('#mark-remove');
+var $inputSubmitButton = $('#input-submit')
+var $markReadButton = $('#mark-read')
+var $markRemoveButton = $('#mark-remove')
 // Input Variables
-var $inputNewTitle = $('#input-title');
-var $inputNewURL = $('#input-url');
+var $inputNewTitle = $('#input-title')
+var $inputNewURL = $('#input-url')
 // Output Variables
-var $outputNewTitle = $('#insert-bookmark-title');
-var $outputNewURL = $('#insert-bookmark-url');
+var $outputNewTitle = $('#insert-bookmark-title')
+var $outputNewURL = $('#insert-bookmark-url')
 // Create New Article
-var $addNewBookmark = $('#add-new-bookmark');
+var $addNewBookmark = $('#add-new-bookmark')
 // var articleContent =
 
 // jQuery
 $inputSubmitButton.on('click', function () {
-  $('#add-new-bookmark').append('<article class="new-bookmark"><li class="new-bookmark-title" id="insert-bookmark-title">Bookmark Title</li><li class="new-bookmark-url" id="insert-bookmark-url">Bookmark URL</li><button type="button" id="mark-read" class="read-bookmark">Read</button><button type="button" id="mark-remove" class="remove-bookmark">Remove</button></article>');
+  $('#add-new-bookmark').append('<article class="new-bookmark"><li class="new-bookmark-title" id="insert-bookmark-title">' +
+$inputNewTitle.val() +
+  '</li><li class="new-bookmark-url" id="insert-bookmark-url">' +
+$inputNewURL.val() +
+ '</li><button type="button" id="mark-read" class="read-bookmark">Read</button><button type="button" id="mark-remove" class="remove-bookmark">Remove</button></article>');
 });
+
+// $outputNewTitle.text($inputNewTitle.val());
+// $outputNewURL.text($inputNewURL.val());
