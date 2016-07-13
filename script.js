0ddfd862933e222a6a29679df687e55eac8839a8
Blake Worsley <blakeworsley@gmail.com>
2016-07-12 20:29:48 -0600

    added read and delete buttons with functionality -broken piece of code in some of our variables that was changed -had to call the read button from the section container and find the #mark-read using the  function -copyed the code and added remove functionality

--- script.js
+++ script.js
@@ -1,33 +1,30 @@
 // Button Variables
 // var inputSubmitButton = document.getElementById('input-submit');
 var $inputSubmitButton = $('#input-submit')
-var $markReadButton = ('#mark-read')
+// var $markReadButton = ('#mark-read')
 var $markRemoveButton = ('#mark-remove')
 // Input Variables
 var $inputNewTitle = $('#input-title')
 var $inputNewUrl = $('#input-url')
-var $inputNewTitleValue = $inputNewTitle.val()
-var $inputNewUrlValue = $inputNewUrl.val()
 // Output Variables
 var $outputNewTitle = $('#insert-bookmark-title')
 var $outputNewUrl = $('#insert-bookmark-url')
 var $newArticle = $('#add-new-article')
 
 
-//user input title and url
-//clicks inputSubmitButton
-  //take input from both inputNewTitle and inputNewUrl
-  //create new article
-  //insert into new outputNewTitle and outputNewURL
-
 $inputSubmitButton.on('click', function() {
+  // make variable that takes input newurl
   $newArticle.append('<article id="add-new-bookmark" class="new-bookmark"><li id="insert-bookmark-title" class="new-bookmark-title">' +
   $inputNewTitle.val() + // input from the user
   '</li><li id="insert-bookmark-url" class="new-bookmark-url">' +
-  $inputNewUrl.val() + //input from the user
+  '<a href=' + $inputNewUrl.val() + '>' + $inputNewUrl.val() + '</a>' + //input from the user
   '</li><button id="mark-read" class="read-bookmark" type="button" name="button">Read</button><button id="mark-remove" class="remove-bookmark" type="button" name="button">Remove</button></article>');
 });
 
-//add html for new article
-  //modify article content / title and url
-  //
+$('#add-new-article').on('click', '#mark-read', function () {
+  $(this).parent().toggleClass('read');
+}); // You cannot target the mark-read button because it doesn't exist when the page loads. I instead toggled the parent that did exist and selected #mark-read from within. The only when to find it again is to use the $(this)
+
+$('#add-new-article').on('click', '#mark-remove', function () {
+  $(this).parent().remove();
+});
