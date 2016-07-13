9b070adbe45e4f24195a1a46fe76f6fe599a2012
Blake Worsley <blakeworsley@gmail.com>
2016-07-13 08:19:19 -0600

    Refactored functions added css flexbox and styling to each bookmark (responsive) added count for each div created (uses an h2 in second section) '

--- script.js
+++ script.js
@@ -1,30 +1,39 @@
 // Button Variables
-// var inputSubmitButton = document.getElementById('input-submit');
+var $bookmarkCount = $('#bookmark-count')
 var $inputSubmitButton = $('#input-submit')
-// var $markReadButton = ('#mark-read')
-var $markRemoveButton = ('#mark-remove')
-// Input Variables
 var $inputNewTitle = $('#input-title')
 var $inputNewUrl = $('#input-url')
-// Output Variables
 var $outputNewTitle = $('#insert-bookmark-title')
 var $outputNewUrl = $('#insert-bookmark-url')
 var $newArticle = $('#add-new-article')
 
-
+// Submit Function
 $inputSubmitButton.on('click', function() {
-  // make variable that takes input newurl
-  $newArticle.append('<article id="add-new-bookmark" class="new-bookmark"><li id="insert-bookmark-title" class="new-bookmark-title">' +
-  $inputNewTitle.val() + // input from the user
-  '</li><li id="insert-bookmark-url" class="new-bookmark-url">' +
-  '<a href=' + $inputNewUrl.val() + '>' + $inputNewUrl.val() + '</a>' + //input from the user
-  '</li><button id="mark-read" class="read-bookmark" type="button" name="button">Read</button><button id="mark-remove" class="remove-bookmark" type="button" name="button">Remove</button></article>');
+    var $count = $('article').length + 1
+    if ($('#input-title').val() <= 0) {
+        return alert('Please Enter a Title');
+    };
+    if ($('#input-url').val() <= 0) {
+        return alert('Please Enter a URL');
+    };
+    $newArticle.append('<article id="add-new-bookmark" class="new-bookmark">' +
+        '<li id="insert-bookmark-title" class="new-bookmark-title">' +
+        $inputNewTitle.val() + // input from the user
+        '</li>' +
+        '<li id="insert-bookmark-url" class="new-bookmark-url">' +
+        '<a href=' + $inputNewUrl.val() + '>' + $inputNewUrl.val() + '</a>' + //input from the user
+        '</li>' +
+        '<button id="mark-read" class="read-bookmark" type="button" name="button">Read</button>' +
+        '<button id="mark-remove" class="remove-bookmark" type="button" name="button">Remove</button></article>');
+    $('#bookmark-count').text('Total Bookmarks: ' + $count);
 });
 
-$('#add-new-article').on('click', '#mark-read', function () {
-  $(this).parent().toggleClass('read');
+// Read Button
+$('#add-new-article').on('click', '#mark-read', function() {
+    $(this).parent().toggleClass('read');
 }); // You cannot target the mark-read button because it doesn't exist when the page loads. I instead toggled the parent that did exist and selected #mark-read from within. The only when to find it again is to use the $(this)
 
-$('#add-new-article').on('click', '#mark-remove', function () {
-  $(this).parent().remove();
+// Remove Button
+$('#add-new-article').on('click', '#mark-remove', function() {
+    $(this).parent().remove();
 });
