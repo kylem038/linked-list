8583ea76c4c7655b8e164a43e510f6fbccfd278f
Blake Worsley <blakeworsley@gmail.com>
2016-07-14 08:09:29 -0600

    refactored all functions

--- script.js
+++ script.js
@@ -3,97 +3,83 @@ var $bookmarkCount = $('#bookmark-count')
 var $inputSubmitButton = $('#input-submit')
 var $inputNewTitle = $('#input-title')
 var $inputNewUrl = $('#input-url')
-var $outputNewTitle = $('#insert-bookmark-title')
-var $outputNewUrl = $('#insert-bookmark-url')
 var $newArticle = $('#add-new-article')
+var $bookmarkReadCount = $('#bookmark-read-count')
+var linkCount = 0;
 
-function titleInputEmpty() {
-  return $('#input-title').val() <= 0;
-}
-
-function urlInputEmpty() {
-  return $('#input-url').val() <= 0;
-}
-
-function bookmarkTemplate(newUrl, newTitle) {
-  return '<article id="add-new-bookmark" class="new-bookmark">' +
-            '<li id="insert-bookmark-title" class="new-bookmark-title">' +
-              '<a href=' + newUrl + 'target="_blank" class="bookmark-title-link">' + newTitle + '</a>' +
-            '</li>' +
-            '<li id="insert-bookmark-url" class="new-bookmark-url">' +
-              '<a href=' + newUrl +  'target="_blank">' + newUrl + '</a>' +
-            '</li>' +
-            '<button id="mark-read" class="read-bookmark" type="button" name="button">Read</button>' +
-            '<button id="mark-remove" class="remove-bookmark" type="button" name="button">Remove</button>' +
-          '</article>';
-}
-
-// Submit Function
-$inputSubmitButton.on('click', function() {
-    var $count = $('article').length + 1
-    $('#bookmark-count').text('Total Bookmarks: ' + $count);
-    // if ($('#input-title').val() <= 0) {
-    if (titleInputEmpty()) { return alert('Please Enter a Title'); }
-    // $('#input-url').val() <= 0)
-    if (urlInputEmpty()) { return alert('Please Enter a URL'); }
-    var bookmarkHTML = bookmarkTemplate($inputNewUrl.val(), $inputNewTitle.val());
-    $newArticle.append(bookmarkHTML);
-    // $newArticle.append('<article id="add-new-bookmark" class="new-bookmark">' +
-    //     '<li id="insert-bookmark-title" class="new-bookmark-title"><a href=' +
-    //     $inputNewUrl.val() + 'target="_blank" class="bookmark-title-link">' +
-    //     $inputNewTitle.val() + // input from the user
-    //     '</a>' +
-    //     '</li>' +
-    //     '<li id="insert-bookmark-url" class="new-bookmark-url">' +
-    //     '<a href=' + $inputNewUrl.val() +  'target="_blank">' + $inputNewUrl.val() + '</a>' + //input from the user
-    //     '</li>' +
-    //     '<button id="mark-read" class="read-bookmark" type="button" name="button">Read</button>' +
-    //     '<button id="mark-remove" class="remove-bookmark" type="button" name="button">Remove</button></article>');
-
-});
-
-// Read Button
-$('#add-new-article').on('click', '#mark-read', function() {
-    $(this).parent().toggleClass('read');
-    var $countRead = $('.read').length
-    $('#bookmark-read-count').text('Total Read Bookmarks: ' + $countRead);
-}); // You cannot target the mark-read button because it doesn't exist when the page loads. I instead toggled the parent that did exist and selected #mark-read from within. The only when to find it again is to use the $(this)
-
-function updateBookmarkCount() {
-  var $downCount = $('article').length;
-  $('#bookmark-count').text('Total Bookmarks: ' + $downCount);
-}
-
-// Remove Button
-$('#add-new-article').on('click', '#mark-remove', function() {
-    $(this).parent().remove();
-    // updateBookmarkCount
-    // var $downCount = $('article').length
-    // $('#bookmark-count').text('Total Bookmarks: ' + $downCount);
-    updateBookmarkCount();
-    // updateReadBookmarkCount
-    var $downCountRead = $('.read').length
-    $('#bookmark-read-count').text('Total Read Bookmarks: ' + $downCountRead);
-});
-
-// Enable Submit Button
-function $toggleSubmitButton() {
-  if ($('#input-title').val() === '' || $('#input-url').val() === '') {
-    $inputSubmitButton.prop('disabled', true);
-  }
-  if  ($('#input-title').val().length > 0 || $('#input-url').val().length > 0) {
-    $inputSubmitButton.prop('disabled', false);
-  }
-}
 $(document).ready($toggleSubmitButton);
 $inputNewTitle.on('keyup', $toggleSubmitButton);
 $inputNewUrl.on('keyup', $toggleSubmitButton);
 
+function bookmarkTemplate(newUrl, newTitle) {
+  return '<article id="add-new-bookmark" class="new-bookmark">' +
+    '<li id="insert-bookmark-title" class="new-bookmark-title">' +
+    '<a href="' + newUrl + '" target="_blank" class="bookmark-title-link">' + newTitle + '</a>' +
+    '</li>' +
+    '<li id="insert-bookmark-url" class="new-bookmark-url">' +
+    '<a href= "' + newUrl + '" target="_blank">' + newUrl + '</a>' +
+    '</li>' +
+    '<button id="mark-read" class="read-bookmark" type="button" name="button">Read</button>' +
+    '<button id="mark-remove" class="remove-bookmark" type="button" name="button">Remove</button>' +
+    '</article>';
+}
+
+// Submit Function
+$inputSubmitButton.on('click', function() {
+  linkCount++;
+  updateBookmarkCount();
+  var bookmarkHTML = bookmarkTemplate($inputNewUrl.val(), $inputNewTitle.val());
+  $newArticle.append(bookmarkHTML);
+});
+
+function titleInputEmpty() {
+  return $inputNewTitle.val() <= 0;
+}
+
+function urlInputEmpty() {
+  return $inputNewUrl.val() <= 0;
+}
+
+// Remove Button
+$newArticle.on('click', '#mark-remove', function() {
+  linkCount--
+  $(this).parent().remove();
+  updateBookmarkCount();
+  updateReadCount();
+});
+
+function updateBookmarkCount() {
+  $bookmarkCount.text('Total Bookmarks: ' + linkCount);
+}
+
+function updateReadCount() {
+  var $downCountRead = $('.read').length;
+  $bookmarkReadCount.text('Total Read Bookmarks: ' + $downCountRead);
+}
+
+// Read Button
+$newArticle.on('click', '#mark-read', function() {
+  $(this).parent().toggleClass('read');
+  updateReadCount();
+});
+
+function inputBoxesAreEmpty() {
+  return $inputNewTitle.val() === '' || $inputNewUrl.val() === '';
+}
+
+// Enable Submit Button
+function $toggleSubmitButton() {
+  if (inputBoxesAreEmpty()) {
+    $inputSubmitButton.prop('disabled', true);
+  } else {
+    $inputSubmitButton.prop('disabled', false);
+  }
+}
+
 // Clear Read Class
-$('#clear-read').on('click', function () {
+$('#clear-read').on('click', function() {
   $('.read').remove();
-  var $downCount = $('article').length
-  var $downCountRead = $('.read').length
-  $('#bookmark-count').text('Total Bookmarks: ' + $downCount);
-  $('#bookmark-read-count').text('Total Read Bookmarks: ' + $downCountRead);
+  linkCount = $('article').length;
+  updateBookmarkCount();
+  updateReadCount();
 });
